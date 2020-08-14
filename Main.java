ee8ce6fc6edf381e0d8c4b9383151ff05955119c
tp69463 <69691537+tp69463@users.noreply.github.com>
2020-08-14 10:38:14 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Tammy.");
+		System.out.println("My favorite color is blue.");
+		System.out.println("My favorite TV show at the moment is The Office.");
+		System.out.println("I am considering a major in biomedical engineering.");
+		System.out.println("My dream college is UCLA.");
+	}
+}
